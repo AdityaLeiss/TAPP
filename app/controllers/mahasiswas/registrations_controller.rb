63b5ddd4629aba1e1f4a92834deb79114c9eb7cb
AdityaLeiss <adityaputra368@gmail.com>
2019-08-29 10:32:55 +0800

    logical patch

--- app/controllers/mahasiswas/registrations_controller.rb
+++ app/controllers/mahasiswas/registrations_controller.rb
@@ -39,6 +39,16 @@ class Mahasiswas::RegistrationsController < Devise::RegistrationsController
   # end
 
   # protected
+  protect_from_forgery with: :exception
+
+    before_filter :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
+        def configure_permitted_parameters
+            devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:idm, :email, :password) }
+            devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:idm, :email, :password) }
+        end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
