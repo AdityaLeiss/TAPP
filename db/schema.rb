08ae0ab870f0bc859a163e4a26a8229ff5aff8aa
AdityaLeiss <adityaputra368@gmail.com>
2019-08-30 11:45:05 +0800

    Add superadmin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_28_031643) do
+ActiveRecord::Schema.define(version: 2019_08_29_063846) do
 
   create_table "admins", force: :cascade do |t|
     t.string "nama"
@@ -72,6 +72,18 @@ ActiveRecord::Schema.define(version: 2019_08_28_031643) do
     t.string "status"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "supers", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_supers_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_supers_on_reset_password_token", unique: true
   end
 
   create_table "users", force: :cascade do |t|
